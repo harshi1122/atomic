7b704ffdea3a061196b039edcc492f57537ce34d
Ryan <jesselockard99@gmail.com>
2022-02-17 01:45:05 -0500

    impl ColorModeAtom

--- src/context/color_mode.ts
+++ src/context/color_mode.ts
@@ -1,15 +1,36 @@
-import { atom } from 'recoil'
+import { atom, useRecoilState, useSetRecoilState } from 'recoil'
 
-import { LocalStorageEffect } from '../util'
+import {
+  LocalStorageEffect,
+  MatchMediaEffect,
+  SetElementClassEffect,
+} from '../util'
 
 // --
 
-export type ColorMode = 'bright' | 'dim'
+export type ColorMode = 'light' | 'dark'
 
 // --
 
 export const ColorModeAtom = atom<ColorMode>({
   key: 'atomic.colorMode',
   default: undefined,
-  effects: [LocalStorageEffect<ColorMode>('atomic.colorMode')],
+  effects: [
+    LocalStorageEffect('atomic.colorMode'),
+    MatchMediaEffect('prefers-color-scheme: dark', 'light', 'dark'),
+    SetElementClassEffect(document.body),
+  ],
 })
+
+// --
+
+/**
+ * Convinence hook for accessing the `ColorModeAtom` using an API similar to `useState`.
+ */
+export const useColorMode = () => useRecoilState(ColorModeAtom)
+
+/**
+ * Convinence hook for accessing a setter, which can be used to update the `ColorModeAtom`,
+ * **without** subscribing to changes to its state.
+ */
+export const useSetColorMode = () => useSetRecoilState(ColorModeAtom)
--- src/context/index.ts
+++ src/context/index.ts
@@ -1,1 +1,2 @@
+export * from './color_mode'
 export * from './styler'
