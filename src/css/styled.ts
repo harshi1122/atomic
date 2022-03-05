43c124da170e56b2a65ca1bf9dc449a45fa7dc43
Ryan <jesselockard99@gmail.com>
2022-03-05 02:37:32 -0500

    created StyledComponent type

--- src/css/styled.ts
+++ src/css/styled.ts
@@ -1,3 +1,6 @@
+import { styled } from 'goober'
 export { css, styled } from 'goober'
 export type { CSSAttribute as CSSObject } from 'goober'
 export { createGlobalStyles } from 'goober/global'
+
+export type StyledComponent = ReturnType<ReturnType<ReturnType<typeof styled>>>
